aa1213e3a9b9358d6cd8789b50e50c294dd75c60
Sagar Kamble <58131131+ksagar09@users.noreply.github.com>
2021-10-18 06:23:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDoApp from './ToDo Project/component/todoProject/todo'
+
+ReactDOM.render(
+ 
+  <>
+  <ToDoApp />
+</>,
+document.getElementById('root')
+);
+
